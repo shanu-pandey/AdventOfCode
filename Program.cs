d7b06044c021e0cd72696e30799c0d99f0b54df7
Shantanu Pandey <shantanu.067@gmail.com>
2025-01-13 20:19:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode._2025;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Day1 day1 = new Day1();
+         
+        var day1_part1_res = day1.TotalDistance_Part1();
+        Console.WriteLine($"Day 1 Part 1 Result is : {day1_part1_res}");
+
+        var day1_part2_res = day1.SimilarityScore_Part2();
+        Console.WriteLine($"Day 1 Part 2 Result is : {day1_part2_res}");
+    }
+}
